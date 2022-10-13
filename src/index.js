c9ae8d078f15b3aed7982c3b76209170008e60b4
Raycris Maldonado <raycris.maldonado@utreee.com>
2022-10-13 14:48:44 -0400

    use context

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { UserContextProvider } from "./context/useContext";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <UserContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </UserContextProvider>
 );
