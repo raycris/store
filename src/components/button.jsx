fa41daf6ac4fa8daa679f10d4e3d75eb7d6e58f4
Raycris Maldonado <raycris.maldonado@utreee.com>
2022-10-13 12:09:16 -0400

    form with stripe card

---
+++ src/components/button.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Button = () => {
+  return (
+    <Container>
+        Buy
+    </Container>
+  )
+}
+
+export default Button
+
+const Container = styled.button`
+border-radius: 6px;
+border: none;
+width: 50%;
+height: 42px;
+color: #ffffff;
+font-weight: bold;
+background-color: #006340;
+`
